0a73d636d64e40aee48ab48b9081120ce3b0b6a5
ingifranco <giovanni.franco12@tin.it>
2016-01-10 19:20:58 +0100

    Esercizio 3 - gestione parametri in applicazione console con relativa visualizzazione

---
+++ Esercizio 3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_3
+{
+    class Utility
+    {
+        static public void Ferma()
+        {
+            Console.Write("\nPremi un tasto per continuare ...");
+            Console.ReadKey();
+        }
+
+        static public void Alt()
+        {
+            Console.Clear();
+            Console.Write("\nPremi un tasto per continuare ...");
+            Console.ReadKey();
+        }
+
+        static public void Inizia()
+        {
+            Console.Clear();
+            Console.WriteLine();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPar = args.Length;
+
+            Utility.Inizia();
+
+            if(numPar == 0)
+            {
+                Console.WriteLine("Non ci sono parametri in ingresso!!");
+
+            }
+            else
+            {
+                // ... Loop with the foreach keyword.
+                int idx = 1;
+                foreach (string value in args)
+                {
+                    Console.Write("parametro n.{0}: ", idx++);
+                    Console.Write("{0,20}", value);
+                    Console.WriteLine(" - {0,-16:X}", value.GetHashCode());
+                }
+            }
+
+
+
+            Utility.Ferma();
+        }
+    }
+}
