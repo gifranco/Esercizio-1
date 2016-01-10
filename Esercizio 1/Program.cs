ae31bf079d28cc5e3b852d9f1fc859ad39197477
ingifranco <giovanni.franco12@tin.it>
2016-01-10 18:38:29 +0100

    Inserito il secondo esercizio

---
+++ Esercizio 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_1
+{
+    class Stop
+    {
+        static public void Ferma()
+        {
+            Console.Write("\nPremi un tasto per continuare ...");
+            Console.ReadKey();
+        }
+
+        static public void Alt()
+        {
+            Console.Clear();
+            Console.Write("\nPremi un tasto per continuare ...");
+            Console.ReadKey();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Buongiorno a tutti");
+            Stop.Ferma();
+            Stop.Alt();
+        }
+
+        public void es1()
+        {
+            int 
+        }
+    }
+}
